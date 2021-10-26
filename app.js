7d59e990d384781cc4c168d81e83a94ed7e73f6b
Jessica.W <jessica.watson1@live.com>
2021-10-26 10:17:39 -0600

    learning about node

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+//test test 
+console.log('Hello World!')
+
+//core js
+console.log([`one`, `two`, `three`].slice(1))
+console.log(Math.random())
+
+//browser only
+// console.log(document)
+
+//global objects in node
+console.log(global)
+console.log(__filename)
+console.log(__dirname)
+// console.log(require)
+console.log(process.argv)
