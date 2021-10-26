57cde362129d9e7b52997d28a43ff9827ddff218
Jessica.W <jessica.watson1@live.com>
2021-10-26 12:37:24 -0600

    talked about built in modules

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 
-
+//What is in node
 //test test 
-console.log('Hello World!')
+// console.log('Hello World!')
 
-//core js
-console.log([`one`, `two`, `three`].slice(1))
-console.log(Math.random())
+// //core js
+// console.log([`one`, `two`, `three`].slice(1))
+// console.log(Math.random())
 
-//browser only
-// console.log(document)
+// //browser only
+// // console.log(document)
 
-//global objects in node
-console.log(global)
-console.log(__filename)
-console.log(__dirname)
-// console.log(require)
-console.log(process.argv)
+// //global objects in node
+// console.log(global)
+// console.log(__filename)
+// console.log(__dirname)
+// // console.log(require)
+// console.log(process.argv)
+
